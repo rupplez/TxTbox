56128d1515c345a99350af9353827c70d80bf25f
rupplez <rupplezzz@gmail.com>
2021-01-10 03:57:40 +0900

    init commit

---
+++ demo/demo/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "txtbox.h"
+
+using namespace std;
+
+int main()
+{
+	printText("Hello, TxTbox!\n");
+
+	return 0;
+}
---
+++ txt_box/txt_box/tFuncs.cpp
@@ -0,0 +1,7 @@
+#include "txtbox.h"
+
+using namespace std;
+
+void printText(string str) {
+	cout << str << endl;
+}
